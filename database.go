f96db89675576d0cd425c89ba8da929120e40b82
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-01-18 16:45:04 +0800

    [platform] update db to wait for db connection case missing

--- database.go
+++ database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 )
 
 /*
@@ -15,12 +16,30 @@ func GetDatabase(dbAddress string, dbUsername string, dbPassword string, dbName 
 
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
 		dbUsername, dbPassword, dbAddress, dbName)
+
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
-		log.Fatalf("ERROR GetDatabase sql open connection fatal error: %v", err)
+		log.Printf("ERROR GetDatabase sql open connection fatal error: %v", err)
+		for {
+			log.Println("INFO GetDatabase re-attempting to reconnect to database...")
+			time.Sleep(1 * time.Second)
+			db, err = sql.Open("mysql", dataSourceName)
+			if err == nil {
+				break
+			}
+		}
 	}
 	if err = db.Ping(); err != nil {
-		log.Fatalf("ERROR GetDatabase db ping fatal error: %v", err)
+		log.Printf("ERROR GetDatabase db ping fatal error: %v", err)
+		for {
+			log.Println("INFO GetDatabase re-attempting to reconnect to database...")
+			time.Sleep(1 * time.Second)
+			db, err = sql.Open("mysql", dataSourceName)
+			err2 := db.Ping()
+			if err == nil && err2 == nil {
+				break
+			}
+		}
 	}
 	log.Printf("INFO GetDatabase database connection: established successfully with %s\n", dataSourceName)
 	return db

beded2c015a19ad83bdfce4138eb6e4a710530ee
kuroyamii <gerysastrawan123@gmail.com>
2022-05-01 23:20:41 +0800

    Added createNewImagekitClient and UploadToImagekit

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/SIC-Unud/sicgolib
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/codedius/imagekit-go v1.1.1
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.1
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- imagekit_utils.go
+++ imagekit_utils.go
@@ -1,11 +1,60 @@
 package sicgolib
 
 import (
+	"context"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
+
+	"github.com/codedius/imagekit-go"
 )
+
+/*
+createNewImagekitClient return an imagekit client and an error.
+The function will need public key and private key from desired imagekit in order to use the function.
+*/
+func createNewImagekitClient(publicKey string, privateKey string) (*imagekit.Client, error) {
+	opts := imagekit.Options{
+		PublicKey:  publicKey,
+		PrivateKey: privateKey,
+	}
+
+	imageKit, err := imagekit.NewClient(&opts)
+	if err != nil {
+		log.Println("ERROR (ImageKit): Error while creating imagekit client options")
+		return imageKit, err
+	}
+	return imageKit, nil
+}
+
+/*
+UploadToImagekit returns an imagekit Upload Response and an error that you can store if you need to know the upload responses and the error.
+The function will need some parameters: context, public key, private key, byte image File, file name, destination folder name on imagekit.
+You can only upload 1 image with this function according to imagekit rules. You need to use looping if you want to upload more than 1 file.
+*/
+func UploadToImagekit(ctx context.Context, publicKey string, privateKey string, file []byte, fileName string, folderName string) (*imagekit.UploadResponse, error) {
+	ur := imagekit.UploadRequest{
+		File:              file,
+		FileName:          fileName,
+		UseUniqueFileName: false,
+		Tags:              []string{},
+		Folder:            folderName,
+		IsPrivateFile:     false,
+		CustomCoordinates: "",
+		ResponseFields:    nil,
+	}
+	client, err := createNewImagekitClient(publicKey, privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	upr, err := client.Upload.ServerUpload(ctx, &ur)
+	if err != nil {
+		log.Println(upr)
+	}
+	return upr, nil
+}
 
 /*
 ParseImageFile returns byte file, multipart file header, and an error.

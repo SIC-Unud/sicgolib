1820ef9c64f6467b8a534cc293cedbde8d5fb143
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2022-05-05 00:11:16 +0800

    [#4] Fix  on GetDatabase funtion

--- database.go
+++ database.go
@@ -9,17 +9,22 @@ import (
 
 /*
 GetDatabase returns database connected to a programmatically defined database connection.
-It throws an error if connection fails in a Fatal manner, because database connection is essential to almost any backend
+It not throws an error if connection fails in a Fatal manner
+But this function will wait 1 second before retrying to connect to db again
+Because database connection is essential to almost any backend
 */
 func GetDatabase(dbAddress string, dbUsername string, dbPassword string, dbName string) *sql.DB {
 	log.Printf("INFO GetDatabase database connection: starting database connection process")
 
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
-		dbUsername, dbPassword, dbAddress, dbName)
+																dbUsername, dbPassword, dbAddress, dbName)
 
-	var db *sql.DB
+	var (
+		db *sql.DB
+		err error
+	)
 	for {
-		db, err := sql.Open("mysql", dataSourceName)
+		db, err = sql.Open("mysql", dataSourceName)
 		if pingErr := db.Ping(); err != nil || pingErr != nil {
 			if err != nil {
 				log.Printf("ERROR GetDatabase sql open connection fatal error: %v\n", err)
